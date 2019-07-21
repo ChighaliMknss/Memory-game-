dbeef7a23af96c81ab107a93308f7a846befc475
Chighali Mouknass <chighalimouknass@chighalis-mbp.home>
2019-07-21 13:28:01 -0500

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+
+var cardOne = "queen";
+var cardtwo = "king";
+var cardthree = "queen";
+var cardFour = "king";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardFour);
